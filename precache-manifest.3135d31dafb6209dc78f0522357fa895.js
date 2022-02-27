0df31429d5899dcf8dd84245e6ad36709d84a15e
KaishAli <360mohdkaishali.cs@gmail.com>
2022-02-27 17:30:12 +0530

    Updates

---
+++ precache-manifest.3135d31dafb6209dc78f0522357fa895.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f535c13a9fff9855f4f8",
+    "url": "/react-advance-TicTacToe/static/css/main.ea21a0ca.chunk.css"
+  },
+  {
+    "revision": "f535c13a9fff9855f4f8",
+    "url": "/react-advance-TicTacToe/static/js/main.f535c13a.chunk.js"
+  },
+  {
+    "revision": "174e69608a51e35fd3b7",
+    "url": "/react-advance-TicTacToe/static/js/1.174e6960.chunk.js"
+  },
+  {
+    "revision": "543e335cdab24bf7c6ae",
+    "url": "/react-advance-TicTacToe/static/js/runtime~main.543e335c.js"
+  },
+  {
+    "revision": "d22ff24049680ba6ad33660be22be2bc",
+    "url": "/react-advance-TicTacToe/index.html"
+  }
+];
